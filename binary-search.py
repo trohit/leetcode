24a9559352d8d4df8e2116540436e605ac786083
Rohit <trohit@users.noreply.github.com>
2021-12-22 22:35:47 +0530

    Create binary-search.py

---
+++ binary-search.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/binary-search
+'''
+Simple to grasp the concept but notoriously hard to get right
+https://ai.googleblog.com/2006/06/extra-extra-read-all-about-it-nearly.html
+https://stackoverflow.com/questions/6372037/binary-search-problems
+https://www.geeksforgeeks.org/problem-binary-search-implementations/
+
+'''
+def xprint(*args, **kwargs):
+    return
+    print("".join(map(str,args)), **kwargs)
+    
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        si = 0
+        ei = len(nums) -1
+        while si <= ei:
+            mi = si + (ei-si)//2
+            xprint(f"{target} {si} {mi} {ei}")
+            if nums[mi] > target:
+                ei = mi-1
+            elif nums[mi] < target:
+                si=mi+1
+            elif nums[mi] == target:
+                return mi
+            else:
+                return mi+1
+        return -1
+        
