a78ebb2da1a99c8334a1ffdfc17cd4700ed1d119
Rohit <trohit@users.noreply.github.com>
2021-12-22 19:00:26 +0530

    Update sqrtx.py

--- unsolved/sqrtx.py
+++ unsolved/sqrtx.py
@@ -23,3 +23,33 @@ class Solution:
             i+=1
         return -1
         
+# another attempt
+'''
+times out and doesnt work for 2147483647
+'''
+def xprint(*args, **kwargs):
+    return
+    print("".join(map(str,args)), **kwargs)
+
+def binsearch(x):
+    st = 2
+    end = x//2
+    while st <= end:
+        mid = int(st + (end-st)/2)
+        sq = mid*mid
+        xprint(f"x:{x} sq: {sq} st:{st} mid:{mid} end:{end}")
+        # import pdb;pdb.set_trace()
+        if sq > x:
+            xprint("<")
+            end = mid-1
+        elif sq < x:
+            xprint(">")
+            st = mid+1
+    return mid
+    
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # edge cases
+        if x == 0 or x==1 or x==2 : return x
+        res = binsearch(x)
+        return res
