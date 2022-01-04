f7727ef1423b3e6366a5d88a0de7a13ca32c3c5c
Rohit <trohit@users.noreply.github.com>
2022-01-04 22:10:23 +0530

    Update lowest-common-ancestor-of-a-binary-tree-iii.py

--- lowest-common-ancestor-of-a-binary-tree-iii.py
+++ lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -33,7 +33,10 @@ class Solution:
             tp = tp.parent
         
 '''
-Better
+Option - 2
+Better runtime : uses a set to check for dup elements and reduces O(m*n) to O(m+n) 
+Space: O(m)
+Time : O(m+n*O(1)) = O(m+n)
 Runtime: 72 ms, faster than 54.26% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree III.
 Memory Usage: 18.6 MB, less than 34.98% of Python3 online submissions for Lowest Common Ancestor of a Binary Tree III.
 '''
@@ -62,3 +65,39 @@ class Solution:
             if q in pll:
                 return q
             q = q.parent
+
+            
+'''
+Option - 3 : Uses 2 pointer method like the same method used to find intersection of linkedlists.
+say len(p) = 3, len(q) = 5
+say last 2 nodes are common
+a1--------->
+(p)        \
+            -> c4 ->c5
+(q)        /
+b1->b2->b3
+so start both pointers at the head of p1 and q1
+after moving 3 nodes, p1 hits the end. then make p1 point to q.(b1)
+after moving 5 nodes, p2 hits the end. then make it point to p.(a1)
+
+at some point both of them will have traversed the same number of nodes.
+Space: O(1)
+Time : O(m+n)
+'''
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        a = p
+        b = q
+        while a != b:
+            a = q if a == None else a.parent
+            b = p if b == None else b.parent    
+        return a
+    
