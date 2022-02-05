9f44782786e88744bd6c831e8dc873d0438e1829
Rohit <trohit@users.noreply.github.com>
2022-02-05 11:02:51 +0530

    Update remove-nth-node-from-end-of-list.py

--- arrays/remove-nth-node-from-end-of-list.py
+++ arrays/remove-nth-node-from-end-of-list.py
@@ -48,3 +48,19 @@ class Solution:
         return head
         
         
+'''
+A faster way
+'''
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        size = 1
+        cur = p = head
+        while cur.next:
+            size += 1
+            cur = cur.next
+            if size > n + 1:
+                p = p.next
+        if size == n:
+            return head.next
+        else:
+            p.next = p.next.next
+            return head
