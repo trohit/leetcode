d09656c91f2f4144206e24aacfaf32f315adec16
Rohit <trohit@users.noreply.github.com>
2022-02-06 11:29:29 +0530

    Update longest-substring-without-repeating-characters.py

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -1,35 +1,50 @@
 """
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
-Runtime: 672 ms
-Memory Usage: 14.4 MB
+3. Longest Substring Without Repeating Characters
+Given a string s, find the length of the longest substring without repeating characters.
 
-Time: O(n) as if all the elements are the same, and the string len is 100, pgm will examine all 100 elems with some elems being examined again 
-Space: O(k) where k is the number of unique elems in the stringand in worst case k=n 
-"""
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Runtime: 99 ms, faster than 44.31% of Python3 online submissions for Longest Substring Without Repeating Characters.
+Memory Usage: 14 MB, less than 92.05% of Python3 online submissions for Longest Substring Without Repeating Characters.
+
+Time complexity : O(2n) = O(n). In the worst case each character will be visited twice by left and right.
+Space complexity : O(min(m, n)) = O(min(m,n)). Same as the previous approach. We need O(k)O(k) space for the sliding window, where kk is the size of the Set.
+The size of the Set is upper bounded by the size of the string nn and the size of the charset/alphabet mm."""
+'''
+1. keep 2 ptrs left and right, initially both start at 0
+2. keep track of how many times each char is seen
+3. keep expanding right ptr until it reaches the end of the string
+4. keep contracting left ptr and updating stats until dup chars not seen.
+5. once no dup chars, at each stage, record the max uniq len seen.
+'''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        mlen=cnt=i=0
-        ss=dict()
-        while i < len(s):
-            #print(f"{i} {s[i]} {mlen}")
-            if s[i] not in ss:
-                ss[s[i]]=i
-                cnt+=1
-                i+=1
-            else:
-                #print("dup")
-                if mlen<cnt:
-                    mlen=cnt
-                    #print(f"newm:{mlen}")
-                
-                oldpos=ss[s[i]]
-                ss.clear()
-                #ss[s[i]]=i
-                cnt=0
-                i=oldpos+1
-            #print(f"{s[i]} {cnt}")
-        #print(f"{mlen} {cnt}")
-        mlen=max(mlen,cnt)
-        return mlen
-            
-        
+        n, left, right, max_len = len(s), 0, 0, 0
+        chars = [0] * 128    # index where char last seen
+        while right < n:
+            r = s[right]
+            print(f"l:{left} r:{right} [{s[left:right+1]}] max_len:{max_len}")
+            chars[ord(r)] += 1
+            while chars[ord(r)] > 1:
+                l = s[left]
+                chars[ord(l)] -= 1
+                left += 1
+                print("contracting left")
+            max_len = max(max_len, right - left + 1)
+            right += 1
+        return max_len
