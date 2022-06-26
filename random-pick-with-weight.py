7a8392d15c48b4611f1f834689aeb89ce2df2e5f
Rohit <trohit@users.noreply.github.com>
2022-06-26 08:57:31 +0530

    Update random-pick-with-weight.py

--- random-pick-with-weight.py
+++ random-pick-with-weight.py
@@ -33,6 +33,37 @@ class Solution:
         print(r)
         for i in range(len(self.hv)):
             if r <= self.hv[i]:
+                
+'''
+*********************
+# bit faster approach
+*********************
+Runtime: 7944 ms, faster than 8.04% of Python3 online submissions for Random Pick with Weight.
+Memory Usage: 18.9 MB, less than 12.76% of Python3 online submissions for Random Pick with Weight.
+'''
+class Solution:
+
+    def __init__(self, w: List[int]):
+        # make weights
+        self.parr = [x/sum(w) for x in w]
+
+        # make absolute vals in l
+        self.l = [0]*len(self.parr)
+        prev_sum = 0
+        for k,v in enumerate(self.parr):
+            self.l[k] = prev_sum + self.parr[k]
+            prev_sum = self.l[k]
+        
+    def pickIndex(self) -> int:
+        rnd = random.random()
+        # chek for 1st index that is less than or equal to values in abs arr l
+        for k,v in enumerate(self.l):
+            if rnd <= self.l[k]:
+                return k
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
                 return i
         print("something bad hapened")
         
