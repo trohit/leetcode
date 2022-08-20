47e1ccacba435627d854bfc772686cd225020424
Rohit <trohit@users.noreply.github.com>
2022-08-20 09:34:01 +0530

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -24,3 +24,27 @@ class Solution:
                 return [i,dd[buddy_val]]
             dd[v] = i #store pos of the value
         
+'''
+Sub-optimal soltution just for comparison
+Note that it needs to pass these 2 cases:
+[3,2,4]
+[3,3]
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        :type nums List[int]
+        :type target int
+        :rtype List[int]
+        """
+        dd_inv = {} # maps val->pos
+        for k,v in enumerate(nums):
+            dd_inv[v] = k
+        for k,v in enumerate(nums):            
+            # print(dd_val_to_pos)
+            cval = target - v
+            if cval in dd_inv:
+                tp = dd_inv[cval]
+                if tp != k:
+                    return [k,tp]
+        
