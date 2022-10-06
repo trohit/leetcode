3f2928d47fa6f50b9bdeab3555c1b6f79449421f
Rohit <trohit@users.noreply.github.com>
2022-10-06 16:02:54 +0530

    Update best_time_to_buy_and_sell_stock.py

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -1,14 +1,21 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 # Input: prices = [7,1,5,3,6,4]
 # Output: 5
+"""
+T: O(n)
+S: O(1)
+https://www.youtube.com/watch?v=1pkOgXD63yU
+"""
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2:
-            return 0
-        max_profit = max(0, prices[1] - prices[0])
-        min_price = prices[0]
-        for p in prices[1:]:
-            max_profit = max(p - min_price, max_profit)
-            min_price = min(p, min_price)
-        return max_profit
-        
+        n = len(prices)
+        a = prices
+        max_prof = 0
+        l, r = 0, 1
+        while r < n:
+            t_prof = a[r] - a[l] 
+            # print(f"{t_prof} = a[{r}] - a[{l}]")
+            if t_prof < 0: l = r
+            else: max_prof = max(t_prof, max_prof)
+            r+=1
+        return max_prof
