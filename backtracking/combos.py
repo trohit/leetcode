3f8dd5c560186bee99cca52bf04091081ca3db48
Rohit <trohit@users.noreply.github.com>
2022-10-10 16:44:38 +0530

    Update combos.py

--- backtracking/combos.py
+++ backtracking/combos.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
-from typing import List
+"""
 # n choose k
 # T: O(k.2^n)
+n = 4
+k = 2
+# of combos = n!/(n-k)!.k!
+
+Output:
+[[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]] 
+"""
+
+from typing import List
 def helper(i:int, sl:list[int], res:list[int], n:int, k:int)->None:
     # base
     if len(sl) == k:
