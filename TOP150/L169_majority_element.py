651ea1b703bceca915fd3aa9c3203455ba6be705
Rohit <trohit@users.noreply.github.com>
2024-03-15 17:18:52 +0530

    Update and rename L69_majority_element.py to L169_majority_element.py

--- TOP150/L169_majority_element.py
+++ TOP150/L169_majority_element.py
@@ -1,4 +1,7 @@
 """
+https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
+169
+
 Given an array nums of size n, return the majority element.
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
